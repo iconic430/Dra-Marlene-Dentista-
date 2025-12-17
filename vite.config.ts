ffae77b4b44ea6162b4a4012be75cf8cb926024f
iconic430 <santiagonorco@gmail.com>
2025-12-17 14:16:15 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+
+// Esto arregla el error de "__dirname" en proyectos modernos
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => {
-    base: './',
     const env = loadEnv(mode, '.', '');
     return {
+      base: './', // <--- Ahora sí está en el lugar correcto
       server: {
         port: 3000,
         host: '0.0.0.0',
